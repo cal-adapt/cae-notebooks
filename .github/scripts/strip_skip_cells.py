73f77770765aeecbf43ebb5b3b65808f34c17eba
Nicole Keeney <nicolejkeeney@gmail.com>
2026-03-13 23:13:09 -0300

    refactor: remove skip-cell stripping, run notebooks directly with papermill

--- .github/scripts/strip_skip_cells.py
+++
@@ -1,8 +0,0 @@
-"""Remove cells tagged 'skip' from a notebook before passing to papermill."""
-import sys
-import nbformat
-
-input_path, output_path = sys.argv[1], sys.argv[2]
-nb = nbformat.read(input_path, as_version=4)
-nb.cells = [c for c in nb.cells if "skip" not in c.get("metadata", {}).get("tags", [])]
-nbformat.write(nb, output_path)
